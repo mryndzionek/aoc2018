184ee8da008d1674b89a92af3575d33399480a40
Mariusz Ryndzionek <mryndzionek@gmail.com>
2018-12-01 08:39:41 +0100

    Small naming adjustment

--- src/Days/Days.hs
+++ src/Days/Days.hs
@@ -10,8 +10,8 @@ day1 :: Str -> (Integer, Integer)
 day1 (Str input) = let freq = map read $ lines $ filter (/= '+') input :: [Integer]
                        freqs = cycle freq
                        freqSt (_, f', fs) f = let nf = (f + f') in (Set.member nf fs, nf, Set.insert nf fs)
-                       freqSts = map (\(a, b, _) -> (a, b)) $ scanl freqSt (False, 0, Set.empty) freqs
-                       fstTwice = snd . head $ dropWhile (not . fst) freqSts
+                       freqSets = map (\(a, b, _) -> (a, b)) $ scanl freqSt (False, 0, Set.empty) freqs
+                       fstTwice = snd . head $ dropWhile (not . fst) freqSets
                    in (sum freq, fstTwice)
 
 solutions :: Map.Map Int (IO Solution)
