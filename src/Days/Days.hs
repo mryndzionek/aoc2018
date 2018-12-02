d05e548f3fab8f00f0d5bebd663df804adfa4399
Mariusz Ryndzionek <mryndzionek@gmail.com>
2018-12-02 07:41:47 +0100

    Added solution to day 2

--- src/Days/Days.hs
+++ src/Days/Days.hs
@@ -13,6 +13,17 @@ day1 (Str input) = let freq = map read $ lines $ filter (/= '+') input
                        fstTwice = snd . head $ dropWhile (not . fst) freqSets
                    in (sum freq, fstTwice)
 
+day2 :: Str -> (Int, String)
+day2 (Str input) = let ids = lines input
+                       counts c = map fst . (count :: [Int] -> [(Int, Integer)]) . map snd $ count c
+                       find n = filter ((n `elem`) . counts) ids
+                       checksum = let (twos, threes) = (find 2, find 3) in length twos * length threes
+                       markDiffering = zipWith (\c1 c2 -> if c1 == c2 then c1 else '?')
+                       correct = filter ('?' /=) . head $ filter ((== 1) . length . filter ('?' ==)) [markDiffering a b | a <- ids, b <- ids, a /= b]
+                   in  (checksum, correct)
+
 solutions :: Map.Map Int (IO Solution)
 solutions = Map.fromList [
-    (  1, mkDay (day1, fileToStr "inputs/day1.txt", (454, 566) ))]
+    (  1, mkDay (day1, fileToStr "inputs/day1.txt", (454, 566) )),
+    (  2, mkDay (day2, fileToStr "inputs/day2.txt", (8610, "iosnxmfkpabcjpdywvrtahluy") ))
+    ]
