61f95131a6d0278bf2f782430bad31405c8988a5
Mariusz Ryndzionek <mryndzionek@gmail.com>
2018-12-09 17:57:37 +0100

    Switched to a more elegant solution

--- src/Days/Day8.hs
+++ src/Days/Day8.hs
@@ -2,56 +2,49 @@ module Days.Day8
   ( day8
   ) where
 
-import Control.Lens
-import Data.Function (on)
+import Control.Monad
+import Data.Either (fromRight)
+import Data.Maybe (mapMaybe)
+import Data.Void
 import qualified Data.List.Safe as S
-import Data.List.Split
-import qualified Data.Map.Strict as Map
-import Data.Maybe (fromMaybe)
+
+import Text.Megaparsec hiding (State, parse)
+import Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer as Lx
 
 import Util
 
-data Node = Node
-  { __id :: Int
-  , _level :: Int
-  , _metadata :: [Int]
-  , __children :: [Int]
-  } deriving (Show, Eq, Ord)
+type Parser = Parsec Void String
 
-makeLenses ''Node
+lexeme :: Parser a -> Parser a
+lexeme = Lx.lexeme space
 
-rec :: Int -> Int -> [Int] -> ([Node], Int, [Int])
-rec i lev es
-  | null es = ([], 0, [])
-  | head es == 0 =
-    let len = (head $ tail es)
-        meta = take len (drop 2 es)
-        rest = drop len (drop 2 es)
-     in ([Node i lev meta []], i + 1, rest)
-  | otherwise =
-    let (n, i', r) = rec i (lev + 1) (drop 2 es)
-        h = (head es - 1) : [head $ tail es]
-        (n', i'', r') = rec i' lev (h ++ r)
-        nodes = S.sortBy (compare `on` view level) (n ++ n')
-        parent = head nodes
-        childr = __id $ head (tail nodes)
-        nodes' = tail nodes ++ [parent & _children %~ ([childr] ++)]
-     in (nodes', i'', r')
+integer :: Parser Int
+integer = lexeme Lx.decimal
+
+sum1 :: Parser Int
+sum1 = do
+  nChild <- integer
+  nMeta <- integer
+  childr <- sum <$> replicateM nChild sum1
+  meta <- sum <$> replicateM nMeta integer
+  pure $ childr + meta
+
+sum2 :: Parser Int
+sum2 = do
+  nChild <- integer
+  nMeta <- integer
+  childr <- replicateM nChild sum2
+  meta <- replicateM nMeta integer
+  pure $
+    if null childr
+      then sum meta
+      else sum . mapMaybe (\i -> childr S.!! (i - 1)) $ meta
+
+parse :: Parser Int -> String -> Either (ParseError Char Void) Int
+parse p = runParser p ""
 
 day8 :: Str -> (Int, Int)
 day8 (Str input) =
-  let entries = (read <$> splitOn " " input)
-      nodes = rec 0 0 entries ^. _1
-      nodeMap = Map.fromList (map (\n -> (__id n, n)) nodes)
-      licNum1 = sum $ concatMap (view metadata) nodes
-      sumN n =
-        if null (__children n)
-          then sum (n ^. metadata)
-          else let ids =
-                     map ((S.!!) (__children n) . (\x -> x - 1)) (n ^. metadata)
-                   nds = map (>>= f) ids
-                   f i = do
-                     n' <- Map.lookup i nodeMap
-                     return $ sumN n'
-                in sum $ map (fromMaybe 0) nds
-   in (licNum1, sumN $ last nodes)
+  let resp = (,) <$> parse sum1 input <*> parse sum2 input
+   in fromRight (0, 0) resp
